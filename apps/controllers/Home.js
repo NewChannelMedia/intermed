73728996096b8539ca51b5f7d161c42f4aa27a7d
oscarSlack <oscar.m@newchannel.mx>
2015-08-13 16:15:31 -0500

    Modificaciones se agrego la linea para JSON DINAMICO
    se agrego el router la linea con la que se puede crear un Json
    dinámico, se agrego la vista para la presentación de las búsquedas y en
    el controlador Home se borraron los métodos que se tenían para hacer
    pruebas

--- apps/controllers/Home.js
+++ apps/controllers/Home.js
@@ -1,25 +1,49 @@
 /**
-* 	En este archivo se atraparan los eventos de la plantilla home.handlebars, 
+* 	En este archivo se atraparan los eventos de la plantilla home.hbs,
 *	asi como también hara la función de recibir y enviar los datos del modelo,
 *	hacía la vista. Se trabajara con una función que simulara como la clase de este
 *	archivo y dentro de esta función es como se trabajara.
-*	Dentro de los metodos iran las llamadas a los metodos o llamados de los modelos y el envio de los parametros 
+*	Dentro de los metodos iran las llamadas a los metodos o llamados de los modelos y el envio de los parametros
 *	@author Oscar, Cinthia.
 *	@version 0.0.0.0
 */
 //librerias que se utilizaran en este archivo
+//var model = require('/../models/Medicos');
 module.exports = {
+	/**
+	*	En el siguiente metodo es el que se encargara del envio
+	*	que se obtengan de la base de datos, se mandaran hacia la
+	*	vista.
+	*/
+	index:function(object, req, res ){
+		res.render('home', object)
+	},
 	sayHello: function(object, req, res) {
-		//console.log("Esto es un arreglo "+ object);
-		res.render('home', {arreglo: object}, function(err, html){ res.send(html)});
-		//if(next) next();	
+		res.render('home', {}, function(err, html){ res.send(html)});
 	},
-	sayBye: function(req, res ) {
-		console.log("Bye");	
-		//if(next) next();
+	vacio: function(object, req, res) {
+		res.render('searchMedic', {}, function(err, html){ res.send(html)});
 	},
-	prueba: function( req, res){
-		console.log("Prueba");
-		//if(next) next();
+
+	// metodo para atrapar los post enviados cuando se eliga especialidad
+	searchEspecialidad: function( object, req, res )
+	{
+		// Todo hacer la busqueda de medicos por especialidad
+		res.render( ' searchMedic', {}, function( err, html ){ res.send(html) });
+	},
+
+	// metodo para atrapar el post y enviarlo al modelo para generar la consulta
+	// para la busqueda de medicos, por nombre.
+	searchMedico: function( object, req, res )
+	{
+		// Todo hacer la busqueda por el nombre del medico
+		res.render('searchMedic',{}, function(err, html){res.send(html)});
+	},
+
+	// metodo para hacer la consulta para buscar por padecimientos se envia los datos al modelo
+	searchPadecimiento: function( object, req, res )
+	{
+		//Todo hacer la busqueda por padecimientos
+		res.render( 'searchMedic', {}, function( err, html ){ res.send(html)} );
 	}
 }
