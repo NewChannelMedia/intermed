dc0855a05397e6ba019414d51bd196a0bf8e0313
Juan Carlos Medina <jcmedina@newchannel.mx>
2015-10-22 16:06:40 -0500

    navigator y nav-menu

--- public/js/utils.js
+++ public/js/utils.js
@@ -1810,3 +1810,18 @@ function socketManejadores() {
 
   } );
 }
+
+function sticky_relocate() {
+    var window_top = $(window).scrollTop();
+    var div_top = $('#sticky-anchor').offset().top;
+    if (window_top > div_top) {
+        $('#sticky').addClass('stick');
+    } else {
+        $('#sticky').removeClass('stick');
+    }
+}
+
+$(function () {
+    $(window).scroll(sticky_relocate);
+    sticky_relocate();
+});
