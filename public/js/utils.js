2a216a0cb1ce79d98a2e47b7d6aff8f6e8bbc301
Juan Carlos Medina <jcmedina@newchannel.mx>
2015-10-26 18:35:39 -0600

    header

--- public/js/utils.js
+++ public/js/utils.js
@@ -1815,9 +1815,9 @@ function sticky_relocate() {
     var window_top = $(window).scrollTop();
     var div_top = $('#sticky-anchor').offset().top;
     if (window_top > div_top) {
-        $('#sticky').addClass('stick');
+        $('.sticky').addClass('stick');
     } else {
-        $('#sticky').removeClass('stick');
+        $('.sticky').removeClass('stick');
     }
 }
 
