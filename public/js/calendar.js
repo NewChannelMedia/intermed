457e189291b5bbae061f54f98eebab9e912a56eb
CarlosAndres1978 <carlosandres1978@gmail.com>
2015-11-17 20:08:24 -0600

    Corrección Horarios

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -1,12 +1,15 @@
+﻿var eventos = [];
+var fechaInicio = '2015-11-02';
+
 $(document).ready(function () {
-
-
     var valido = true;
+    var eventos = JSON.parse($('#horariosUbi').val());
+    console.log(eventos);
 
-    // page is now ready, initialize the calendar...
-
+    //inicializar calendario
     $('#calendar').fullCalendar({
         // put your options and callbacks here
+
         defaultView: 'agendaWeek',
         height: 350,
         allDaySlot: false,
@@ -19,21 +22,21 @@ $(document).ready(function () {
             left: false
         },
         //businessHours: {
-        //    start: '6:00',
-        //    end: '19:00',
+        //    start: '2015-11-08 10:00',
+        //    end: '2015-11-08 12:00',
         //    dow: [0, 1, 2, 3, 4, 5, 6]
         //},
         minTime: '8:00',
         maxTime: '19:00',
-        //defaultDate: '2015-02-12',
         lang: 'es',
+        defaultDate: fechaInicio,
+        events: eventos,
         selectable: true,
         selectHelper: true,
         displayEventTime: false,
+        eventOverlap:false,
         select: function (start, end) {
             var eventData;
-            console.log(start.format('DMYYYY'));
-            console.log(end.format('DMYYYY'));
             if (start.format('DMYYYY') != end.format('DMYYYY')) {
                 alert('fechas distintas');
                 valido = false;
@@ -57,35 +60,42 @@ $(document).ready(function () {
         eventLimit: true,
 
         eventClick: function (event, jsEvent, view) {
-            $('#calendar').fullCalendar('removeEvents', event._id);
+            if (confirm('Desea eliminar el evento?')) {
+                $('#calendar').fullCalendar('removeEvents', event._id);
+            }
+            
 
+        },
+        eventResize: function (event, delta, revertFunc, jsEvent, ui, view) {
+            if (event.start.format('DMYYYY') != event.end.format('DMYYYY')) {
+                alert('fechas distintas');
+                revertFunc();
+            }
         },
         //eventMouseover: function (event, jsEvent, view) {
         //    $(this).append('<span id=\"' + event._id + '\">Clic para eliminar</span>');
 
         //},
-        //eventMouseout: function (event, jsEvent, view) {
-        //    $('#' + event._id).remove();
-        //},
+        eventMouseout: function (event, jsEvent, view) {
+            $('#' + event._id).remove();
+        },
         //eventDrop: function (event, delta, revertFunc, jsEvent, ui, view) {
         //    console.log(jsEvent.target.id);
         //}
 
         eventMouseover: function (event, jsEvent, view) {
-            $(this).append('<img src="img/eliminar.png" id=\"' + event.id  + '\"/>');
+            $(this).append('<img src="img/eliminar.png" id=\"' + event.id + '\"/>');
 
         },
         eventMouseout: function (event, jsEvent, view) {
             $('#' + event.id).remove();
         }
-    })
 
-    //al cambiar de tab mostrar el calendario la usar bootstrap
-    $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
-        $('#calendar').fullCalendar('render');
-    });
-    $('#tabControl a:first').tab('show');
+    })
 });
+
+
+
 
 function obtenerHorarios() {
     var objhorarios = [];
@@ -96,23 +106,25 @@ function obtenerHorarios() {
     var fin = new moment();
 
     objhorarios = [];
-    for (i = 0; i <= h.length-1; i++) {
+    for (i = 0; i <= h.length - 1; i++) {
         evento = h[i];
-        
+
         inicio.hours(evento.start._d.getUTCHours());
         inicio.minute(evento.start._d.getUTCMinutes());
 
         fin.hours(evento.end._d.getUTCHours());
         fin.minute(evento.end._d.getUTCMinutes());
-                
+
+        console.log($('#direccion_id').val());
+
         var horario = {
-            direccion_id: $('#idDireccion').val(),
+            direccion_id: $('#direccion_id').val(),
             dia: evento.start._d.getDay(),
             horaInicio: inicio.format('HH:mm'),
             horaFin: fin.format('HH:mm')
         };
-        
-        objhorarios.push(horario);        
-    };    
+
+        objhorarios.push(horario);
+    };
     return objhorarios;
 };
