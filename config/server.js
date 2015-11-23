125fcdd6a3f891609135f4265bc690178a9f1a76
oscarSlack <oscar.m@newchannel.mx>
2015-11-23 11:33:13 -0600

    Cambios

--- config/server.js
+++ config/server.js
@@ -2,7 +2,7 @@
 
 //arranca el servidor
 var server = function ( ap, port ) {
-  return require( 'socket.io' ).listen( ap.listen( port ) );
+  return require( 'socket.io' ).listen( ap.listen( port, '0.0.0.0' ) );
 };
 
 //se importa para que otros js lo puedan ejecutar
