a56ce5785972aeba1d331d749522cc6c56a9e9ec
oscarSlack <oscar.m@newchannel.mx>
2015-12-22 14:45:50 -0600

    Avance

--- apps/controllers/createSites.js
+++ apps/controllers/createSites.js
@@ -1,104 +1,195 @@
  var fs = require('fs');
  var models = require('../models');
  module.exports ={
-   creaArchivo: function(req,res){
-     var html = "";
-     var html2 = "";
-     var d = new Date();
-     var fechaCompleta = d.getFullYear()+"-"+(d.getMonth()+1)+"-"+d.getDate();
-     //si existe
-     fs.exists('sitemapindex.xml', function(exists){
-       if( !exists ){
-         fs.exists('sitemap'+fechaCompleta+'.xml', function( existe ){
-           if( !existe ){
-             //crea los archivos
-             if( fs.openSync('sitemap'+fechaCompleta+'.xml','w') ){//crea el sitemap
-               if( fs.openSync('sitemapindex.xml','w') ){// crea el sitemap index
-                 //maqueta el sitemap index
-                 html += '<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n';
-                  html += '\t<sitemap>\n';
-                    html += '\t\t<loc>'+'http://www.intermed.online/'+'sitemap'+fechaCompleta+'.xml'+'</loc>\n';
-                    html += '\t\t<lastmod>'+fechaCompleta+'</lastmod>\n';
-                  html += '\t</sitemap>\n';
-                 html += '</sitemapindex>\n';
-                 //escribe en el archivo sitemapindex
-                 fs.writeFile('sitemapindex.xml',html,'utf8',function(err){
-                   if(err){
-                     console.log("Error al escribir el archivo: "+err);
-                   }else{
-                     console.log("Creado y escrito exitosamente");
-                   }
-                 });
-                 //consulta para traer la informacion que va a ir dentro de sitemap
-                 models.Usuario.findAll({
-                   where:{tipoUsuario:'M'},
-                   attributes:['usuarioUrl','urlPersonal']
-                 }).then(function(usuario){
-                   usuario = JSON.parse(JSON.stringify(usuario));
-                   //html2 se usa para maquetar la informacion de sitemap
-                   html2 += '<?xml version="1.0" encoding="UTF-8"?>\n';
-                   html2 += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n';
-                   for( var i in usuario ){
-                     //condicion para checar que sea menor de 50 mil url
-                     if( i < 50000 ){
-                       // condicion para checar si el usuario trae url personalizada
-                       if( usuario[i].urlPersonal && usuario[i].urlPersonal.length > 0 ){
-                         html2 += '\t<url>\n';
-                           html2 += '\t\t<loc>http://www.intermed.online/'+usuario[i].urlPersonal+'</loc>\n';
-                           html2 += '\t\t<lastmod>'+fechaCompleta+'</lastmod>\n';
-                           html2 += '\t\t<changefreq>weekly</changefreq>\n';
-                           html2 += '\t\t<priority>1.0</priority>\n';
-                         html2 += '\t</url>\n';
-                       }else{
-                         html2 += '\t<url>\n';
-                           html2 += '\t\t<loc>http://www.intermed.online/'+usuario[i].usuarioUrl+'</loc>\n';
-                           html2 += '\t\t<lastmod>'+fechaCompleta+'</lastmod>\n';
-                           html2 += '\t\t<changefreq>weekly</changefreq>\n';
-                           html2 += '\t\t<priority>1.0</priority>\n';
-                         html2 += '\t</url>\n';
-                       }
-                     }else{
-                       //se crea nuevo archivo y se escribe en sitemapindex la nueva ruta del nuevo sitemap
-
-                     }
-                   }
-                   html2 += '</urlset>';
-                   //var nombre se saca el nombre del sitemap en el cual se va a escribir
-                   var nombre = 'sitemap'+fechaCompleta+'.xml';
-                   //se abre el flujo para escribir lo que se saco de la consulta
-                   fs.writeFile(nombre,html2,'utf8',function(err){
-                     if(err){
-                       console.log("Error a escribir en este archivo");
-                     }else{
-                       console.log("Escrito correctamente");
-                     }
-                   });
-                 });
-               }else{
-                 console.log("no se pudo crear el sitemapindex");
-               }
-             }else{
-               console.log("No se pudo crear el sitemap");
-             }
-           }else{
-             console.log("Archivo sitemap"+fechaCompleta+".xml ya existe");
-           }
-         });
-       }else{
-         console.log("Archivo sitemapindex.xml ya existe");
-       }
-     });
-   },
-   actualizaArchivo: function(file){
-     fs.appendFile(file,'data',function( err ){
-       if( err ){
-         console.log('Err: No agregado');
-       }else{
-         console.log('SI');
-       }
-     });
-   },
-   actualizaIndex: function(){
-
+   creaArchivo: function( req, res ){
+     creaIndex('sitemapindex.xml');
+     creaSite('sitemap.xml');
    }
  }
+ /**
+ *  Todas las tareas separadas en funciones
+ *  <- crear archivo ->
+ *  <- agregar al archivo ->
+ *  <- checar si el archivo existe ->
+ *  <- tamaño del archivo <- de 10000 ->
+ *  <- menos de 50 000 registros ->
+ **/
+
+  /**
+  * funcion para crear el sitemapindex
+  * @param name nombre que llevara el archivo
+  * @return message if success
+  **/
+  function creaIndex(name){
+    if( fs.openSync(name,'w') ){
+      console.log("Index creado");
+    }else{
+      console.log("Fallo en la creacion del index");
+    }
+  }
+  /**
+  * funcion para la creacion del sitemap
+  * @param name nombre que llevara el archivo
+  * @return message if success
+  */
+  function creaSite(name){
+    if( fs.openSync(name, 'w') ){
+      console.log("Sitemap creado");
+    }else{
+      console.log("Fallo en la creacion del sitemap");
+    }
+  }
+  /**
+  * La siguiente funcion determina si alguno de los archivos ya existe, en caso de que asi sea
+  * llamara a la funcion correspondiente: Ejem. Si el archivo ya existe, checara que contenga
+  * valores registrados en caso de que no sea así se registraran, para que no se cree una copia
+  * si alguno de los archivos no existe, lo va a crear y registrarle valores, e ir haceindo el correspondiente
+  * registro. Si ninguno de los dos archivos no existe los va a crear y registrar sus correspondientes valores
+  * @param name nombre del primer archivo
+  * @param name2 nombre del segundo archivo
+  * @return boolean, en caso de ser verdadero que si existe retornara un true
+  */
+  function existFile( name, name2 ){
+    var primerIndex = false;
+    var segundoSite = false;
+    fs.exist(name, function( exist ){
+      if( exist ){
+        // se manda a llamar a la funcion que checara si esta vacio, y de asi hacerlo
+        // realizara su correspondiente tarea
+        // se pone a true la variable primerIndex
+        primerIndex = true;
+      }else{
+        //llama a la funcion para crear este archivo, y tambien manda a llamar la funcion encargada de llenar de
+        // valores el correspondiente archivo
+
+      }
+    });
+    fs.exist(name2, function( exist ){
+      if( exist ){
+        // el archivo si existe, se manda a llama ra la funcion que se encargara de checar que el documento
+        // no este vacio, en caso de estarlo se llenara con su respectivo datos
+        segundoSite = true;
+      }else{
+        // en este else, si entra aqui quiere decir que el archivo no existe, y que se debe de llenar
+        // ademas de guardar la informacion en el archivo de sitemapindex
+      }
+    });
+    if( primerIndex && segundoSite ){
+      return true;
+    }else{
+      return false;
+    }
+  }
+  /**
+  * Crear contenido en el sitemapindex
+  * la siguiente funcion servira para agregar
+  * informacion al archivo sitemapindex, para
+  * poder ir agregando cada que se dispone de un nuevo
+  * archivo sitemap, irlo registrando en el sitemapindex
+  * @param nameIndex, nombre del archivo al cual se le debe de ingresar la informacion(sitemapindex)
+  * @param nameSi, nombre del site que se va a agregar
+  * @param indice, numero con el cual se hara el registro del sitemap en el index
+  **/
+  function registerSites( nameIndex, indice ){
+    var html = "";
+    var d = new Date();
+    var fechaCompleta = d.getFullYear()+"-"+(d.getMonth()+1)+"-"+d.getDate();
+    html += '<?xml version="1.0" encoding="UTF-8"?>\n';
+    html += '<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n';
+     html += '\t<sitemap>\n';
+       html += '\t\t<loc>'+'http://www.intermed.online/'+nameSi+indice+'.xml'+'</loc>\n';
+       html += '\t\t<lastmod>'+fechaCompleta+'</lastmod>\n';
+     html += '\t</sitemap>\n';
+     fs.writeFile( nameIndex, html, 'utf8', function( err ){
+       if( err )throw err;
+       console.log("Se agrego correctamente");
+     });
+  }
+  /**
+  * Con la siguiente funcion se podrá registrar la informacion en los archivos
+  * sitemap.xml ademas de que se les podra poner un numero aleatorio para
+  * poderlos ir identificando.
+  * @param name, nombre del archivo
+  **/
+  function registerAllSites(name){
+    var html = "";
+    var id; // variable para guardar el ultimo id que se uso
+    var bandera = false; // bandera para cuando sobre pase los 50 mil saber que eso paso, se pondrá en true
+    var fechaCompleta = d.getFullYear()+"-"+(d.getMonth()+1)+"-"+d.getDate();
+    html += '<?xml version="1.0" encoding="UTF-8"?>\n';
+    html += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n';
+    html += '</urlset>';
+    // en esta parte el archivo ya debe de estar creado entonces se puede escribir sobre el
+    fs.writeFile( name , html, 'utf8', function( err ){
+      if( err )throw err;
+      console.log("Se han podido agregar perfectamente los datos al archivo");
+    });
+      // se hace una consulta para traer todos los url de los usuarios medicos
+      models.Usuario.findAll({
+        where:{tipoUsuario:'M'},
+        attributes:['id','usuarioUrl','urlPersonal']
+      }).then(function(usuario){
+        usuario = JSON.parse(JSON.stringify(usuario));
+        for( var i in usuario ){
+          // en caso que el usuario llegue a mas de 50 mil registros, se dejara de agregar la informacion
+          //al archivo y se abrira otro donde se empeza a escribir en el nuevo archivo
+          if( i < 50000 ){
+            // se revisa si el campo urlPersonal contenga datos, en caso de hacer asi se escribe,
+            // la etiqueta loc con la url personalizada en caso contrario se agregara solamente
+            // la url por default
+            if( usuario[i].urlPersonal && usuario[i].urlPersonal.length > 0 ){
+                updateSitemap(name, usuario[i].urlPersonal);
+            }else{
+                updateSitemap( name, usuario[i].usuarioUrl );
+            }
+          }else{
+            // si sobre pasa los 50 mil registros, se va a crear otro archivo sitemap y se volvera a llenar con la nueva informacion
+            console.log("Ultimo id: "+id);
+            bandera = true;
+            // se rompe el for con un break
+            break;
+          }
+        }
+      });
+  }
+  /**
+  * Siguiente funcion servira para agregar un bloque de <sitemap> al xml
+  * sitemapindex, donde se encuentran registrados la ubicacion y diversos
+  * datos sobre los archivos sitemaps.
+  *
+  * @param name, nombre del archivo al cual se le debe de ir agregando la informacion
+  * @param addName, nombre del sitemap a agregar
+  **/
+  function updateIndex( name, addName, fecha ){
+    //se va a ir maquetando la informacion en una variable para pasarla a la funcion
+    // que va a ir agregando todo.
+    var html = "";
+    html += '\t<sitemap>\n';
+      html += '\t\t<loc>http://www.intermed.online/'+addName+'</loc>\n';
+      html += '\t\t<lastmod>'+fecha+'</lastmod>\n';
+    html += '\t</sitemap>';
+    fs.appendFile(name,html, function( err ){
+      if( err )throw err;
+      console.log("El "+name+" fue agregado exitosamente al archivo");
+    });
+  }
+  /**
+  * funcion para la actualizacion del archivo SITEMAP.xml en el cual se podra
+  * agregar un bloque de <url>
+  * @param String name, nombre del archivo al cual se le debe de agregar
+  * @param String valor, esta variable sirve para ir agregando el url
+  */
+  function updateSitemap( name, valor ){
+    // variable para poder maquedar el bloque
+    var html = "";
+    html += '\t<url>';
+      html += '\t\t<loc>http://www.intermed.online/'+valor+'</loc>\n';
+      html += '\t\t<lastmod>'+fechaCompleta+'</lastmod>\n';
+      html += '\t\t<changefreq>weekly</changefreq>\n';
+      html += '\t\t<priority>1.0</priority>\n';
+    html += '\t</url>';
+    fs.appendFile(name,valor, function( err ){
+      if( err )throw err;
+      console.log('se han agregado exitosamente los valores al archivo');
+    });
+  }
