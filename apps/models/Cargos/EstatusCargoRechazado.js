7dbc976e6cc8fd1346711a50c26985c1943962e3
ivan.salcedo@gmail.com <ivan.salcedo@gmail.com>
2016-01-16 13:44:18 -0600

    Cargos rechazados

---
+++ apps/models/Cargos/EstatusCargoRechazado.js
@@ -0,0 +1,22 @@
+﻿"use strict";
+
+module.exports = function (sequelize, DataTypes) {
+    var EstatusCargoRechazado = sequelize.define("EstatusCargoRechazado", {
+        id: { type: DataTypes.BIGINT, autoIncrement: true, primaryKey: true, name: 'idEstatusCobrosRechazados' },
+        descripcion: {type: DataTypes.STRING}
+    },{
+    classMethods: {
+      associate: function(models) {
+        EstatusCargoRechazado.belongsTo(models.CargoRechazado)
+      }
+    }},
+    {
+        timestamps: false,
+        paranoid: true,
+        underscored: true,
+        freezeTableName: true,
+        tableName: 'estatuscargosrechazados'
+    });
+
+    return EstatusCargoRechazado;
+};
