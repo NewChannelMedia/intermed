60f75e64b9d11b454b5975f3395f9bc5124a3d4e
oscarSlack <oscar.m@newchannel.mx>
2016-01-19 18:04:45 -0600

    Avance branch

--- apps/controllers/encryption.js
+++ apps/controllers/encryption.js
@@ -1,25 +1,9 @@
 /**
- * Script para la encriptación, desde passwords hasta
- * archivos completos.
- *
- *
- * @author Oscar
- * @date Thursday, August 27, 2015
- * @version 0.0.0.0
- *
- **/
-// se cargan las librerias necesarias
-var criptosis = require( 'crypto' );
-/**
- * metodo para encriptar el token
- *
- * @param id
- * @param times
- * @return ecript token
- */
-function doEncriptToken( id, times ) {
-  var concatenando = id + times;
-  return String( criptosis.createHash( 'md5' ).update( concatenando ).digest( 'hex' ) );
-}
-// funciones que se podrán exportar
-exports.doEncriptToken = doEncriptToken;
+* El siguiente controlador: Tiene la funcionalidad
+* para poder iniciar session en este controlador
+* ademas de las funciones para encriptar sus archivos
+* y desencriptarlo.
+*
+*
+*
+**/
