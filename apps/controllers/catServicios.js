90feba45739a092b4da45c5019666b5a3d6338bf
Juan Carlos Medina <jcmedina@newchannel.mx>
2016-01-21 16:44:24 -0600

    Revert "Merge branch 'dev2_040116_CatServUbicacion' into development"
    This reverts commit 6538ea5cf481cac0df4055902e16cabbfe68ff84, reversing
    changes made to d697ec600cb8a47e6169200425637babac1e414e.

--- apps/controllers/catServicios.js
+++ apps/controllers/catServicios.js
@@ -45,7 +45,6 @@ module.exports = {
       var usuario_id = req.session.passport.user.id;
       var todo;
       var tipo = "";
-      console.log("Hola servicio");
       tipo = req.body.tipo;
       switch( tipo ){
         case "concepto":todo = {concepto: req.body.valor};break;
