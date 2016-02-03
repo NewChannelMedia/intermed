80811887cdbed4c6df7e2b74a61623fcda713f36
cinthiabNewChannel <cinthia.b@newchannel.mx>
2016-02-02 18:04:49 -0600

    Exportar los códigos de encuestas por tipoCodigo y tipoPlan

---
+++ apps/models/Encuesta/encuestas.js
@@ -0,0 +1,27 @@
+"use strict";
+
+module.exports = function (sequelize, DataTypes) {
+    var DBEncuesta_encuesta = sequelize.define("DBEncuesta_encuesta", {
+        id: { type: DataTypes.BIGINT, autoIncrement: true, primaryKey: true},
+        codigo: { type: DataTypes.STRING, allowNull: false},
+        tipoCodigo: { type: DataTypes.INTEGER, allowNull: false},
+        tipoPlan: { type: DataTypes.INTEGER},
+        canalUsado: { type: DataTypes.INTEGER},
+        codigoUsado: { type: DataTypes.INTEGER},
+        usada: { type: DataTypes.INTEGER},
+        fecha: { type: DataTypes.DATE}
+    }, {
+      classMethods: {
+        associate: function(models) {
+            DBEncuesta_encuesta.belongsTo(models.DBEncuesta_tipoEncuesta,  {foreignKey: 'tipoCodigo'})
+        }
+      },
+      timestamps: false,
+      paranoid: true,
+      underscored: true,
+      freezeTableName: true,
+      tableName: 'encuestasM'
+    });
+
+    return DBEncuesta_encuesta;
+};
