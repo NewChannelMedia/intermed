04266b44a40ba4f7255d2f7d92ff2ee3063b3783
Juan Carlos Medina <jcmedina@newchannel.mx>
2016-02-12 20:29:59 -0600

    home + login modal

---
+++ public/js/home.js
@@ -0,0 +1,38 @@
+/* newHome */
+$(document).ready(function(){
+  if ($('#newHome').length > 0){
+    $('#newHome #newHomeHeader').height(
+      $(window).height()
+    );
+    /*$('#homeMainTitle').css({'margin-top':$(window).height()})*/
+
+    $('#gotocode').click(function(){
+      $('body').scrollTo($('#codigo'), 1000);
+    });
+    $('#bounce').click(function(){
+      $('body').scrollTo($('#homeMainTitle'), 1000);
+    })
+  }
+
+  var bouncetime = 1100;
+  var jumpSize = 35;
+
+  $('#bounce').css({'bottom':jumpSize});
+  bounce();
+
+  function bounce() {
+    $('#bounce').animate({'bottom':20}, bouncetime, 'easeInQuad', function() {
+      $('#bounce').animate({'bottom':jumpSize}, bouncetime, 'easeOutQuad', function() {
+        bounce();
+      });
+    });
+  }
+});
+
+$(window).on('resize', function() {
+  if ($('#newHome').length > 0){
+    $('#newHome #newHomeHeader').height(
+      $(window).height()
+    );
+  }
+});
